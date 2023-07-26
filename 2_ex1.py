6dd8d71df9a22cf96c80474f1e81c3b1c881c178
Luís Jannuário <132780396+DRAC0L1CH@users.noreply.github.com>
2023-07-26 14:14:47 +0100

    2_ex1.py

--- 2_ex1.py
+++ 2_ex1.py
@@ -1,107 +1,102 @@
-""" Função e respetiva derivada  """
-
 def f(x):
-    f=25*x**(4)-x**(2)/2-2
+    f = 25 * x ** 4 - x ** 2 / 2 - 2
     return f
 
 def df(x):
-    df=100*x**(3)-x
+    df = 100 * x ** 3 - x
     return df
 
-
-""" Métodos para determinar a raiz da função """
-
-#Método da bissecção 
 def bisec(a, b, k_max, eps_1, eps_2):
     k = 1
-    c = (1/2)*(a+b)
+    c = (a + b) / 2
     
-    while k < k_max:
-        k += 1
-        c = (1/2)*(a+b)
+    while k <= k_max:
+        print(f"Iteration {k}: a={a}, b={b}, c={c}, f(c)={f(c)}")
         
-        if abs(f(c)) < eps_1 and b-a < eps_2:
-            print(f"Converge em {k} iterações.")
+        if abs(f(c)) < eps_1 and b - a < eps_2:
+            print(f"Converged after {k} iterations.")
             return c
         
-        if f(a)*f(c) < 0:
+        if f(a) * f(c) < 0:
             b = c
         else:
             a = c
         
-    print("Não foi possível encontrar a raiz após", k_max, "iterações.")
+        c = (a + b) / 2
+        k += 1
+    
+    print("Failed to find the root after", k_max, "iterations.")
     return c
 
-
-#Método da Secante 
 def sec(a, b, k_max, eps_1, eps_2):
-    k = 1 
-    c = a + f(a)*((a-b)/(f(b)-f(a)))
-    while k < k_max:
+    k = 1
+    c = a + f(a) * ((a - b) / (f(b) - f(a)))
+    
+    while k <= k_max:
+        print(f"Iteration {k}: a={a}, b={b}, c={c}, f(c)={f(c)}")
+        
+        if abs(f(c)) < eps_1 and b - a < eps_2:
+            print(f"Converged after {k} iterations.")
+            return c
+        
         a = b
         b = c
-        c = a + f(a)*((a-b)/(f(b)-f(a)))
-        if abs(f(c)) < eps_1 and b-a < eps_2:
-            print(f"Converge em {k} iterações.")
-            return c
+        c = a + f(a) * ((a - b) / (f(b) - f(a)))
         k += 1
-    if k == k_max:
-        print("Não foi possível encontrar a raiz após", k_max, "iterações.")
-    else:
-        print("A raiz é:", c)
     
+    print("Failed to find the root after", k_max, "iterations.")
     return c
 
-#Método de Newton 
 def newton(f, df, x0, eps_1, eps_2, k_max):
     x = x0
     k = 1
-    while k < k_max:
+    
+    while k <= k_max:
+        print(f"Iteration {k}: x={x}, f(x)={f(x)}")
+        
         x_prev = x
         x = x - f(x) / df(x)
+        
         if abs(f(x)) < eps_1 and abs(x - x_prev) < eps_2:
-            print(f"Converge em {k} iterações.")
+            print(f"Converged after {k} iterations.")
             return x
+        
         k += 1
-    print(f"Falhou em convergir em {k_max} iterações.")
+    
+    print(f"Failed to converge after {k_max} iterations.")
     return x
 
 
-"""" Escolha do Método Pretendido  """
-
-method = input("Qual o método que deseja utilizar?\n1. Bisseção \n2. Secante \n3. Newton's \nDigite o número correspondente: ")
+method = input("Which method do you want to use?\n1. Bisection\n2. Secant\n3. Newton\nEnter the corresponding number: ")
 
 while method not in ['1', '2', '3']:
-    method = input("Método inválido. Tente novamente.\nQual o método que deseja utilizar?\n1. Bisection method\n2. Secant method\n3. Newton's method\nDigite o número correspondente: ")
+    method = input("Invalid method. Try again.\nWhich method do you want to use?\n1. Bisection\n2. Secant\n3. Newton\nEnter the corresponding number: ")
 
 if method == '1':
-    a = float(input("Digite o valor de a: "))
-    b = float(input("Digite o valor de b: "))
-    k_max = int(input("Digite o número máximo de iterações: "))
-    eps_1 = float(input("Digite o valor de eps_1: "))
-    eps_2 = float(input("Digite o valor de eps_2: "))
-
+    a = float(input("Enter the value of a: "))
+    b = float(input("Enter the value of b: "))
+    k_max = int(input("Enter the maximum number of iterations: "))
+    eps_1 = float(input("Enter the value of eps_1: "))
+    eps_2 = float(input("Enter the value of eps_2: "))
     
     result = bisec(a, b, k_max, eps_1, eps_2)
-    print(f"A raiz da função é: {result}")
+    print(f"The root of the function is: {result}")
 
 elif method == '2':
-    a = float(input("Digite o valor de a: "))
-    b = float(input("Digite o valor de b: "))
-    k_max = int(input("Digite o número máximo de iterações: "))
-    eps_1 = float(input("Digite o valor de eps_1: "))
-    eps_2 = float(input("Digite o valor de eps_2: "))
-
-
+    a = float(input("Enter the value of a: "))
+    b = float(input("Enter the value of b: "))
+    k_max = int(input("Enter the maximum number of iterations: "))
+    eps_1 = float(input("Enter the value of eps_1: "))
+    eps_2 = float(input("Enter the value of eps_2: "))
+    
     result = sec(a, b, k_max, eps_1, eps_2)
-    print(f"A raiz da função é: {result}")
+    print(f"The root of the function is: {result}")
 
 else:
-    x0 = float(input("Digite o valor de x0: "))
-    k_max = int(input("Digite o número máximo de iterações: "))
-    eps_1 = float(input("Digite o valor de eps_1: "))
-    eps_2 = float(input("Digite o valor de eps_2: "))
-
-
-    result =  newton(f, df, x0, eps_1, eps_2, k_max)
-    print(f"A raiz da função é: {result}")
+    x0 = float(input("Enter the value of x0: "))
+    k_max = int(input("Enter the maximum number of iterations: "))
+    eps_1 = float(input("Enter the value of eps_1: "))
+    eps_2 = float(input("Enter the value of eps_2: "))
+    
+    result = newton(f, df, x0, eps_1, eps_2, k_max)
+    print(f"The root of the function is: {result}")
